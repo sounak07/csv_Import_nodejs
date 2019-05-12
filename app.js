4209453f6354b61e691c3daca8c86711b7677c2b
sounak07 <sounakume@gmail.com>
2019-05-12 14:08:11 +0530

    Minor changes in app.js

--- app.js
+++ app.js
@@ -13,9 +13,18 @@ const results = require('./routes/results');
 
 const app = express();
 
+app.use((req, res, next) => {    //CORS 
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS,DELETE');
+  next();
+});
+
+
 mongoose.Promise = global.Promise;
 mongoose
-  .connect('mongodb://sounak:Sounak08*@ds153766.mlab.com:53766/movies-imdb', { useUrlParser: true })
+  .connect('mongodb://sounak:Sounak08*@ds153766.mlab.com:53766/movies-imdb', { useNewUrlParser: true })
   .then(() => console.log('Mongo success'))
   .catch(err => console.log(err));
 
